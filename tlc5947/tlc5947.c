17ef1d68a68f073d0b62ce44870a9b323f9ee107
NoHomey <ivo_stratev@abv.bg>
2016-04-02 15:32:50 +0300

    litle changes hope it fix the bug

--- tlc5947/tlc5947.c
+++ tlc5947/tlc5947.c
@@ -6,15 +6,15 @@
 int tlc5947_write(const uint16_t pwm, const uint8_t chips) {
     const int leds = chips * LIBTLC5947_LEDS;
     const int length = leds + (leds / 2);
-    char buffer[length];
+    unsigned char buffer[length];
     int i, j, fd, written;
     if(pwm > LIBTLC5947_MAXPWM) {
         return -EINVAL;
     }
     for(i = 0; i < leds; i += 2) {
-        buffer[j++] = pwm & 255;
-        buffer[j++] = (pwm & 3840) + (pwm & 15);
-        buffer[j++] = pwm & 4080;
+        buffer[j++] = pwm | 255;
+        buffer[j++] = (pwm | 3840) + (pwm | 15);
+        buffer[j++] = pwm | 4080;
     }
     errno = 0;
     fd = open(LIBTLC5947_DEVICE, O_WRONLY);
@@ -22,7 +22,7 @@ int tlc5947_write(const uint16_t pwm, const uint8_t chips) {
         return -errno;
     }
     errno = 0;
-    written = write(fd, buffer, length);
+    written = write(fd, (char*) buffer, length);
     if(written < 1) {
         close(fd);
         return -errno;
