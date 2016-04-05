3fce82ed78312b7024979ecad4edd5a13f5d8010
Borislav Stratev <bstratev@abv.bg>
2016-04-05 13:20:14 +0300

    adding input testing program

---
+++ input_test.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+#define DEVICE "/dev/input_pins"
+
+int main(void) {
+	int fd = open(DEVICE, O_RDWR);
+	const int length = 4;
+	char read_data[length];
+    char i;
+    char read_length;
+	if(fd == -1) {
+		printf("File %s not exist or is in use.\n", DEVICE);
+		return fd;
+	}
+    while(1) {
+        read_length = read(fd, read_data, length);
+        if(read_length > 0) {
+            for(i = 0; i < read_length; ++i) {
+                printf("pin %d\n", read_data[i]);
+            }
+        }
+    }
+	close(fd);
+
+	return 0;
+}
