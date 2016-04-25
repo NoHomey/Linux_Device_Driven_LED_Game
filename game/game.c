576614a87093200f02795346d41c5014d1a5f13d
NoHomey <ivo_stratev@abv.bg>
2016-04-25 20:52:26 +0300

    adding mocked controlls

--- game/game.c
+++ game/game.c
@@ -1,16 +1,42 @@
 #include <tlc5947.h>
 #include "board.h"
+#include <stdio.h>
+#include <errno.h>
 
 int main(void) {
     struct tlc5947 tlc5947;
 	struct board board;
-    //tlc5947_init(&tlc5947, 8, Wir_BRG);
+	char c;
+    if(tlc5947_init(&tlc5947, 8, Wir_BRG)) {
+		perror("error while initializing struct tlc5947\n");
+		return 1;
+	}
 	board_init(&board, 0, 0);
-	//tlc5947_write(tlc5947);
-	board_move(&board, down);
-	//board_set(board, tlc5947);
-	//tlc5947_write(tlc5947);
-	//tlc5947_free(&tlc5947);
+	tlc5947_write(&tlc5947);
+	while(c != 'y') {
+		c = getchar();
+		switch(c) {
+			case 'w': {
+				board_move(&board, up);
+				break;
+			}
+			case 's': {
+				board_move(&board, down);
+				break;
+			}
+			case 'a': {
+				board_move(&board, left);
+				break;
+			}
+			case 'd': {
+				board_move(&board, right);
+				break;
+			}
+		}
+		board_set(&board, &tlc5947);
+		tlc5947_write(&tlc5947);
+	}
+	tlc5947_free(&tlc5947);
 
     return 0;
 }
