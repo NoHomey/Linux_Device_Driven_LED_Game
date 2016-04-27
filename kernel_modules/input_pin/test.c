ebaf1c7a8b76a9bd13b942b9209932c9a84b7d79
naganu <bstratev@abv.bg>
2016-04-27 06:48:57 +0000

    all buttons tested and works

--- kernel_modules/input_pin/test.c
+++ kernel_modules/input_pin/test.c
@@ -14,7 +14,7 @@ int main(void) {
 		printf("File %s not exist or is in use.\n", DEVICE);
 		return fd;
 	}
-	while(i < 10) {
+	while(i < 20) {
     		read_length = read(fd, read_data, length);
     		if(read_length > 0) {
         		printf("i %d pin %d\n", i++, read_data[0]);
