0b0c82ca754168130e11044e8de5365b5cae9826
NoHomey <ivo_stratev@abv.bg>
2016-04-27 08:34:34 +0300

    adding ioctl

--- kernel_modules/tlc5947/tlc5947.c
+++ kernel_modules/tlc5947/tlc5947.c
@@ -26,7 +26,7 @@ MODULE_SUPPORTED_DEVICE("tlc5947");
 
 static int tlc5947_file_open(struct inode* inode, struct file* file) {
     if(tlc5947_file_opened == 1) {
-        printk(KERN_ERR "Fail to open file /dev/tlc5947\n");
+        printk(KERN_ERR "Fail to open file /dev/%s\n", TLC5947_NAME);
         return -EBUSY;
     }
     tlc5947_file_opened = 1;
@@ -36,13 +36,6 @@ static int tlc5947_file_open(struct inode* inode, struct file* file) {
 
 static ssize_t tlc5947_file_write(struct file* file, const char __user* buffer, const size_t length, loff_t* offset) {
     int i, k;
-    if(!tlc5947_buffer) {
-        tlc5947_buffer = (char*) kmalloc(length * sizeof(char), GFP_KERNEL);
-        if(!tlc5947_buffer) {
-            printk(KERN_ERR "Failed to allocate memmry!\nCalling kmalloc returned NULL\n");
-            return -ENOMEM;
-        }
-    }
     return_value = copy_from_user(tlc5947_buffer, buffer, length);
     if(return_value > 0) {
         printk(KERN_ERR "Error while copying data\nCalling copy_from_user reurned%d\n", return_value);
@@ -64,25 +57,57 @@ static ssize_t tlc5947_file_write(struct file* file, const char __user* buffer, 
 }
 
 static int tlc5947_file_close(struct inode* inode, struct file* file) {
-    if(tlc5947_buffer) {
-        kfree(tlc5947_buffer);
-    }
     tlc5947_file_opened = 0;
 
     return 0;
 }
 
+long tlc5947_ioctl(struct file * file, unsigned int cmd, unsigned long arg) {
+	if(_IOC_TYPE(cmd) != TLC5947_MAGIC_NUMBER) {
+		printk("ioctl magic number isn't device driver's one.\ncmd equals %d\n", cmd);
+		return -ENOTTY;
+	}
+	if(_IOC_NR(cmd) > TLC5947_MAX_NUMBER) {
+		printk("ioctl command exceeds last implemented command.\ncmd equals %d\n", cmd);
+		return -ENOTTY;
+	}
+	switch(cmd) {
+		case TLC5947_FREE: {
+			if(tlc5947_buffer) {
+		        kfree(tlc5947_buffer);
+				tlc5947_buffer = NULL;
+		    }
+			break;
+		}
+		case TLC5947_ALLOC: {
+			if(!tlc5947_buffer) {
+		        tlc5947_buffer = (char*) kmalloc(arg * sizeof(char), GFP_KERNEL);
+		        if(!tlc5947_buffer) {
+		            printk(KERN_ERR "Failed to allocate memmry!\nCalling kmalloc returned NULL\n");
+					tlc5947_exit();
+		            return -ENOMEM;
+		        }
+		    }
+		}
+	}
+
+	return 0;
+}
+
 static int __init tlc5947_init(void) {
     if(tlc5947_data == 255) {
         printk(KERN_ERR "Parameter tlc5947_data value not setted when loading the module\n");
+		tlc5947_exit();
         return -EINVAL;
     }
     if(tlc5947_clock == 255) {
         printk(KERN_ERR "Parameter tlc5947_clock value not setted when loading the module\n");
+		tlc5947_exit();
         return -EINVAL;
     }
     if(tlc5947_latch == 255) {
         printk(KERN_ERR "Parameter tlc5947_latch value not setted when loading the module\n");
+		tlc5947_exit();
         return -EINVAL;
     }
     tlc5947[0].gpio = tlc5947_data;
@@ -91,11 +116,13 @@ static int __init tlc5947_init(void) {
 	return_value = gpio_request_array(tlc5947, TLC5947_GPIOS);
     if(return_value) {
         printk(KERN_ERR "Unable to request GPIOs!\nCalling gpio_request_array returned %d\n", return_value);
+		tlc5947_exit();
         return return_value;
     }
-    return_value = alloc_chrdev_region(&tlc5947_numbers, tlc5947_first_minor, tlc5947_minor_count, "tlc5947");
+    return_value = alloc_chrdev_region(&tlc5947_numbers, tlc5947_first_minor, tlc5947_minor_count, TLC5947_NAME);
     if(return_value) {
        printk(KERN_ERR "Could not allocate device numbers\nCalling alloc_chrdev_region returned %d\n", return_value);
+	   tlc5947_exit();
        return return_value;
     }
     tlc5947_major_number = MAJOR(tlc5947_numbers);
@@ -106,10 +133,12 @@ static int __init tlc5947_init(void) {
     tlc5947_file_operations.open = tlc5947_file_open;
     tlc5947_file_operations.release = tlc5947_file_close;
     tlc5947_file_operations.write = tlc5947_file_write;
+	tlc5947_file_operations.unlocked_ioctl = tlc5947_ioctl;
     tlc5947_cdev->ops = &tlc5947_file_operations;
     return_value = cdev_add(tlc5947_cdev, tlc5947_numbers, tlc5947_minor_count);
     if(return_value) {
         printk(KERN_ERR "Failed to add device numbers to struct cdev\nCalling cdev_add returned %d\n", return_value);
+		tlc5947_exit();
         return return_value;
     }
     tlc5947_file_opened = 0;
@@ -118,10 +147,16 @@ static int __init tlc5947_init(void) {
     return 0;
 }
 
-static void __exit tlc5947_exit(void) {
-    cdev_del(tlc5947_cdev);
-    unregister_chrdev_region(tlc5947_numbers, tlc5947_minor_count);
-    gpio_free_array(tlc5947, TLC5947_GPIOS);
+static void tlc5947_exit(void) {
+	if(tlc5947_cdev) {
+		cdev_del(tlc5947_cdev);
+	}
+	if(tlc5947_major_number > -1) {
+		unregister_chrdev_region(tlc5947_numbers, tlc5947_minor_count);
+	}
+	if((tlc5947[0].gpio == tlc5947_data) && (tlc5947[1].gpio == tlc5947_clock) && (tlc5947[2].gpio == tlc5947_latch)) {
+		gpio_free_array(tlc5947, TLC5947_GPIOS);
+	}
 }
 
 module_init(tlc5947_init);
--- kernel_modules/tlc5947/tlc5947.h
+++ kernel_modules/tlc5947/tlc5947.h
@@ -1,16 +1,22 @@
+#ifndef _TLC5947_H
+#define _TLC5947_H
+
 #include <linux/gpio.h>
 #include <linux/types.h>
 #include <linux/stat.h>
 #include <linux/fs.h>
 #include <linux/cdev.h>
-
-#ifndef _TLC5947_H
-#define _TLC5947_H
+#include <linux/ioctl.h>
 
 #define TLC5947_GPIOS 3
 #define GPIO_HIGH 1
 #define GPIO_LOW 0
+#define TLC5947_NAME "tlc5947"
 #define CONST_PARAM S_IRUSR | S_IRGRP | S_IROTH
+#define TLC5947_MAGIC_NUMBER '~'
+#define TLC5947_ALLOC _IOW(TLC5947_MAGIC_NUMBER, 0, unsigned long)
+#define TLC5947_FREE _IO(TLC5947_MAGIC_NUMBER, 1)
+#define TLC5947_MAX_NUMBER 1
 
 ushort tlc5947_data = 255;
 ushort tlc5947_clock = 255;
@@ -24,18 +30,19 @@ static struct gpio tlc5947[TLC5947_GPIOS] = {
 
 static int return_value;
 static dev_t tlc5947_numbers;
-static int tlc5947_major_number;
+static int tlc5947_major_number = -1;
 static const int tlc5947_first_minor = 0;
 static const unsigned int tlc5947_minor_count = 1;
 static struct file_operations tlc5947_file_operations;
-static struct cdev* tlc5947_cdev;
+static struct cdev* tlc5947_cdev = NULL;
 static int tlc5947_file_opened = 0;
 char* tlc5947_buffer;
 
 static int tlc5947_file_open(struct inode* inode, struct file* file);
 static ssize_t tlc5947_file_write(struct file* file, const char __user* buffer, const size_t length, loff_t* offset);
 static int tlc5947_file_close(struct inode* inode, struct file* file);
+long tlc5947_ioctl(struct file * file, unsigned int cmd, unsigned long arg);
 static int __init tlc5947_init(void);
-static void __exit tlc5947_exit(void);
+static void tlc5947_exit(void);
 
 #endif
